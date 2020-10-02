0b6bab18af5099e4d21655ff88cb6a2ed3a8e55c
lauritzpepke <s191179@student.dtu.dk>
2020-10-02 14:20:51 +0200

    Spiller 1 og Spiller 2 første terningskast

--- src/Spil/Dice.java
+++ src/Spil/Dice.java
@@ -5,14 +5,21 @@ import java.util.Random;
 public class Dice {
 
     public static void main(String[] args) {
-       Random r = new Random();
-       int result = r.nextInt(6);
-        int result2 = r.nextInt(6);
-        result2++;
-       result++;
-        System.out.println("Du rullede " + result);
-        System.out.println("Du rullede " + result2);
-        int sum = result + result2;
-        System.out.println("Samlede resultat " + sum);
-       }
+        Random r = new Random();
+        int førsteresult = r.nextInt(6);
+        int andetresult = r.nextInt(6);
+        andetresult++;
+        førsteresult++;
+        System.out.println("Spiller 1: første terning: " + førsteresult + ". Anden terning: " + andetresult);
+        int sum = førsteresult + andetresult;
+        System.out.println("Spiller 1 rullede i alt: " + sum);
+
+        int førsteresult2 = r.nextInt(6);
+        int andetresult2 = r.nextInt(6);
+        andetresult2++;
+        førsteresult2++;
+        System.out.println("Spiller 2: første terning: " + førsteresult2 + ". Anden terning: " + andetresult2);
+        int sum2 = førsteresult2 + andetresult2;
+        System.out.println("Spiller 2 rullede i alt: " + sum2);
     }
+}
