fa405ea67ad65f52fd2282850bf595b202f69765
Aqibsen <aqibsen@hotmail.com>
2020-10-02 13:52:30 +0200

    Prøved at kode

--- src/Spil/Dice.java
+++ src/Spil/Dice.java
@@ -6,8 +6,13 @@ public class Dice {
 
     public static void main(String[] args) {
        Random r = new Random();
-       int result = r.nextInt(12);
+       int result = r.nextInt(6);
+        int result2 = r.nextInt(6);
+        result2++;
        result++;
         System.out.println("Du rullede " + result);
+        System.out.println("Du rullede " + result2);
+        int sum = result + result2;
+        System.out.println("Samlede resultat " + sum);
        }
     }
