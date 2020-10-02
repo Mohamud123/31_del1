4f62e2d47dc58be34971d4f9f7b10247f566f412
s205347 <s205347@student.dtu.dk>
2020-10-02 13:34:36 +0200

    Funktion med terninger

--- src/Spil/Dice.java
+++ src/Spil/Dice.java
@@ -1,5 +1,13 @@
 package Spil;
 
+import java.util.Random;
+
 public class Dice {
 
-}
+    public static void main(String[] args) {
+       Random r = new Random();
+       int result = r.nextInt(12);
+       result++;
+        System.out.println("Du rullede " + result);
+       }
+    }
